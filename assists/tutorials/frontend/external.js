75300d07162a9d0c868c0386a14fa012d37d680f
Hamit Mızrak <hamitmizrak@gmail.com>
2023-08-11 11:14:34 +0300

    Math

--- assists/tutorials/frontend/external.js
+++ assists/tutorials/frontend/external.js
@@ -18,21 +18,21 @@ sd */
 
 // single thread
 
-var degiskenAdi=44;
-console.log(degiskenAdi);
-console.log(typeof degiskenAdi);
+// var degiskenAdi=44;
+// console.log(degiskenAdi);
+// console.log(typeof degiskenAdi);
 
-var degiskenAdi55=44.23;
-console.log(degiskenAdi55);
-console.log(typeof degiskenAdi55);
+// var degiskenAdi55=44.23;
+// console.log(degiskenAdi55);
+// console.log(typeof degiskenAdi55);
 
-var $degiskenAdi55="Merhabalar Güzel insanlar";
-console.log($degiskenAdi55);
-console.log(typeof $degiskenAdi55);
+// var $degiskenAdi55="Merhabalar Güzel insanlar";
+// console.log($degiskenAdi55);
+// console.log(typeof $degiskenAdi55);
 
-var _$degiskenAdi55=true;
-console.log(_$degiskenAdi55);
-console.log(typeof _$degiskenAdi55);
+// var _$degiskenAdi55=true;
+// console.log(_$degiskenAdi55);
+// console.log(typeof _$degiskenAdi55);
 
 //var 44&~_$degiskenAdi55=true;
 
@@ -40,15 +40,15 @@ console.log(typeof _$degiskenAdi55);
 // var _$degiskenAdi55={};
 
 // Hoisting
-carName="Audi";
-var carName;
+// carName="Audi";
+// var carName;
 
 ///////////////////////////////////////////// 
 // let
-let deneme=44;
-console.log(deneme);
-deneme=66;
-console.log(deneme);
+// let deneme=44;
+// console.log(deneme);
+// deneme=66;
+// console.log(deneme);
 
 // const
 // const deneme99=14525;
@@ -58,25 +58,79 @@ console.log(deneme);
 // Operatör
 // + - * / % ++ --
 // &&=VE ||=VEYA  ! DEĞİL
-let number1=12;
-let number2=5;
-console.log(number1+number2);
-console.log(number1-number2);
-console.log(number1*number2);
-console.log(number1/number2);
-console.log(number1%number2);
+// let number1=12;
+// let number2=5;
+// console.log(number1+number2);
+// console.log(number1-number2);
+// console.log(number1*number2);
+// console.log(number1/number2);
+// console.log(number1%number2);
 
 // =   ATAMA
 // ==  EŞİT (Türüne bakmadan)
 // === EŞİT (Türüne bakarak)
 
 ///////////////////////////////////////////// 
-let userName=Number(prompt("Lütfen sayı giriniz"));
-console.log(userName);
+// let userName=Number(prompt("Lütfen sayı giriniz"));
+// console.log(userName);
 
 ///////////////////////////////////////////// 
-// NaN
+// NaN: Not A Number
+console.log(3/4);
+console.log(3/"asd");
+
 // infinity
+console.log(0/5);
+console.log(5/0);
+
+// undefined
+let data;
+console.log(data);
+
 // new 
 // null
+// zero
+
+///////////////////////////////////////////// 
+// Math 
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.sqrt(25));
+console.log(Math.abs(-25));
+console.log(Math.pow(6,2));
+
+console.log(Math.floor(5.9));
+console.log(Math.ceil(5.1));
+console.log(Math.round(3.4));
+console.log(Math.round(3.5));
+
+console.log(Math.min(4,5,6,1,1000));
+console.log(Math.max(4,5,6,1,1000));
+
+console.log(Math.sin(45));
+console.log(Math.asin(1));
+
+console.log(Math.floor(Math.random()*9+1));
+
+///////////////////////////////////////////// 
+// string 
+
+///////////////////////////////////////////// 
+// Function 
+
+///////////////////////////////////////////// 
+// conditional 
+
+///////////////////////////////////////////// 
+// Loop
+// break return continue
+
+///////////////////////////////////////////// 
+// Dizi
+
+///////////////////////////////////////////// 
+// Object 
+
+///////////////////////////////////////////// 
+// callback, promise, asyn-await
 
