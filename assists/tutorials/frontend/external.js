1483fd93e1ee8d282d180dafbcfe1b306b6b7da2
Hamit Mızrak <hamitmizrak@gmail.com>
2023-08-11 11:18:37 +0300

    Math. Örnekler

--- assists/tutorials/frontend/external.js
+++ assists/tutorials/frontend/external.js
@@ -90,6 +90,23 @@ console.log(data);
 // new 
 // null
 // zero
+/////////////////////////////////////////////
+//Örnek-1
+// Kullanıcıdan aldığımız 2 sayıyı toplayan JS kodu yazınız ?
+
+//Örnek-2
+/* 
+Kullanıcıdan aldığımız sayıyı dereceden fahrenhyat'a çeviren JS kodu yazınız ?
+Formül: (derece*9/5)+32 
+*/
+
+//Örnek-3
+// y=3x+4k ==>1.dereceden 2 bilinmeyenli denklem algoritması
+// Kullanıcı tarafından alınan x ve kdeğerlerini hesaplayan algoritma yazınız ?
+
+// örnek-4 operatör işlemleri: aşağıdaki örneği javascript ile yapalım
+// 4+3*2(3:3-1*6+9:1+(3:3))
+
 
 ///////////////////////////////////////////// 
 // Math 
@@ -111,6 +128,25 @@ console.log(Math.sin(45));
 console.log(Math.asin(1));
 
 console.log(Math.floor(Math.random()*9+1));
+
+// Örnek-4: Aşağıdaki örnekleri math ile çözelim ?
+// -5.9 sayıyının aşağıdaki işlemleri yaptıralım ?
+// 1-mutlak değeri alsın 5.9
+// 2-yuvarlama yapsın  6.0
+// 3-karesini alsın 36.00
+// 4-karekök alsın 6.0
+// 5-yuvarlama yapsın 6.0
+// 6-)çıkan sonucu 5 bölsün 6/5=1
+// 7-) iki sayı arasından karşılaştırma yapsın en küçüğünü alsın ve 1 ve 5
+// 8-) küçük sayı eğer tekse 3 eklesin çiftse 5 eklesin 
+
+// Örnek-5 : kullanıcı tarafından girilen bir sayıyı negatif mi pozitif mi olduğu ekran yazdıran algoritma ?
+// NOT: cast kullalım ?
+
+// Örnek-6 : kullanıcı tarafından password ve repassword alalım sonrasında bu iki değeri karşılaştırma yapalım
+// eğer aynı girilirse aynı veri yoksa birbirine uymadı yazan algoritma yapalım
+
+
 
 ///////////////////////////////////////////// 
 // string 
