d9f611d30c292cd35ad36f2bc318b52e19b7d0c5
Hamit Mızrak <hamitmizrak@gmail.com>
2023-08-11 10:53:44 +0300

    JavaScript

---
+++ assists/tutorials/frontend/external.js
@@ -0,0 +1,82 @@
+//alert("denemöe999")
+/*
+sd
+f
+sdf
+sd
+f
+sd */
+// alert("pop-up1");
+// window.alert("pop-up2");
+// document.writeln("Sayfada göster<br/>");
+// window.document.writeln("Sayfada göster");
+
+// console.log("console log");
+// console.error("console log");
+// console.warn("console log");
+// console.info("console log");
+
+// single thread
+
+var degiskenAdi=44;
+console.log(degiskenAdi);
+console.log(typeof degiskenAdi);
+
+var degiskenAdi55=44.23;
+console.log(degiskenAdi55);
+console.log(typeof degiskenAdi55);
+
+var $degiskenAdi55="Merhabalar Güzel insanlar";
+console.log($degiskenAdi55);
+console.log(typeof $degiskenAdi55);
+
+var _$degiskenAdi55=true;
+console.log(_$degiskenAdi55);
+console.log(typeof _$degiskenAdi55);
+
+//var 44&~_$degiskenAdi55=true;
+
+// var _$degiskenAdi55=[];
+// var _$degiskenAdi55={};
+
+// Hoisting
+carName="Audi";
+var carName;
+
+///////////////////////////////////////////// 
+// let
+let deneme=44;
+console.log(deneme);
+deneme=66;
+console.log(deneme);
+
+// const
+// const deneme99=14525;
+// deneme99=66666;
+
+///////////////////////////////////////////// 
+// Operatör
+// + - * / % ++ --
+// &&=VE ||=VEYA  ! DEĞİL
+let number1=12;
+let number2=5;
+console.log(number1+number2);
+console.log(number1-number2);
+console.log(number1*number2);
+console.log(number1/number2);
+console.log(number1%number2);
+
+// =   ATAMA
+// ==  EŞİT (Türüne bakmadan)
+// === EŞİT (Türüne bakarak)
+
+///////////////////////////////////////////// 
+let userName=Number(prompt("Lütfen sayı giriniz"));
+console.log(userName);
+
+///////////////////////////////////////////// 
+// NaN
+// infinity
+// new 
+// null
+
